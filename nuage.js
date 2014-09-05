020494b03dd25b3f832ac33065fa8b697906fccf
sspringmeyer <steve@springmeyer.com>
2014-09-05 16:53:44 -0700

    put js in a seperate file from html

---
+++ nuage.js
@@ -0,0 +1,43 @@
+var tableauXHeader = "";
+function initializeViz() {
+    var placeholderDiv = document.getElementById("tableauViz");
+    var url = "https://online.tableausoftware.com/t/stratus/views/BGC3Channels/Dashboard3?:embed=y&:display_count=no";
+    var options = {
+        width: placeholderDiv.offsetWidth,
+        height: placeholderDiv.offsetHeight,
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.getActiveSheet();
+        }
+    };
+    viz = new tableauSoftware.Viz(placeholderDiv, url, options);
+}
+/*
+function signinViz() {
+    var signinStr = '<tsRequest><credentials name="steve.springmeyer@bgc3.com" password="Tableau999!"><site contentUrl="https://online.tableausoftware.com/t/stratus"/></credentials></tsRequest>';
+    alert("signin");
+    $.ajax({
+        url: "https://online.tableausoftware.com/t/stratus/api/2.0/auth/signin", //t/stratus",
+        dataType: "text",
+        data: signinStr,
+        type: "POST",
+        mimeType: "text/xml",
+        contentType: "text/xml",
+        success: function (data) {
+            var theaders = this.headers;
+            alert("success >" + data); // X-Tableau-Auth:
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status + " " + thrownError);
+            //console.log(thrownError);
+        }
+    });
+
+}
+*/
+$(document).ready(function () {
+    //signinViz();
+    initializeViz();
+});
